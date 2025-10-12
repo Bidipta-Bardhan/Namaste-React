9f31a485f73184d21fe700ae9dfc462ee52e56cd
Bidipta Bardhan <bidipta.bardhan37@gmail.com>
2025-10-12 22:22:59 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // single element
 // const heading = React.createElement("h1", { id: "heading" }, "Hello World from React");
 /**
@@ -15,15 +18,16 @@
  */
 // This code can get simplified using JSX
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am h1 inside child1"),
-    React.createElement("h2", {}, "I am h2 inside child1"),
+  React.createElement("div", { id: "child1", key: "child1" }, [
+    React.createElement("h1", { key: "h1-child1" }, "I am h1 inside child1"),
+    React.createElement("h2", { key: "h2-child1" }, "I am h2 inside child1"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 inside child2"),
-    React.createElement("h2", {}, "I am h2 inside child2"),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { key: "h1-child2" }, "I am h1 inside child2"),
+    React.createElement("h2", { key: "h2-child2" }, "I am h2 inside child2"),
   ]),
 ]);
+
 console.log(parent) // React Object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Namaste React 
+
+# Parcel
+- dev build
+- local server
+- HMR = Hot Module Replacement (Browser content get refreshed on save)
+- File watching algorithm (build with C++)
+- Caching: Faster Builds
+- Image Optimization
+- For production: minification, bundling, compressing, code splitting
+- Consistent hashing
+- Differential Bundling - supports older browser
+- Diagonistic
+- Error Handling
+- HTTPS
+- Tree Shaking: Remove unused code
+- Different dev and prod build: Dev: npx parcel index.html   prod: npx parcel build index.html
