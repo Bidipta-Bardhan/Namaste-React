dda108305aa59143e28104af1a24c0994a13db7f
Bidipta Bardhan <bidipta.bardhan37@gmail.com>
2025-10-22 00:09:39 +0530

    useState React Hook

--- src/food/components/Body.js
+++ src/food/components/Body.js
@@ -1,13 +1,23 @@
 import resDataArray from "../utils/restaurant-mockdata";
 import RestaurantCard from "./RestaurantCard";
-const Body = () => (
-  <div className="body-container">
-    <div className="search-bar">Search</div>
-    <div className="res-container">
-      {resDataArray.map((resObj) => {
-        return <RestaurantCard key={resObj.info.id} resData={resObj} />;
-      })}
-    </div>
-  </div>
-);
-export default Body
+import { useState } from "react";
+// useState use difference algorithm for virtual dom, which spot difference and render quickly so that   UI and data layer is in sync
+const Body = () => {
+    const [listOfRestaurants,setListOfRestaurants]=useState(resDataArray)
+    return (
+        <div className="body-container">
+            <button className="filter-btn" onClick={() => {
+                const filteredResList = listOfRestaurants.filter(
+                  (item) => item.info.avgRating>4.5
+                );
+                setListOfRestaurants(filteredResList)
+            }} >Top Rated Restaurant</button>
+            <div className="res-container">
+                {listOfRestaurants.map((resObj) => {
+                    return <RestaurantCard key={resObj.info.id} resData={resObj} />;
+                })}
+            </div>
+        </div>
+    )
+};
+export default Body;
